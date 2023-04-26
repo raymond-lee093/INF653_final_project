a24087f08316a1b37efd8ef45fc3311863a1980c
Raymond Lee <raymondlee093@gmail.com>
2023-04-26 16:42:28 -0500

    included engines property, modified CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,3 @@
-const allowedOrigins = [
-  "http://127.0.0.1:3500",
-  "http://localhost:3500",
-  "https://dazzling-snickerdoodle-777101.netlify.app",
-];
+const allowedOrigins = ["https://dazzling-snickerdoodle-777101.netlify.app"];
 
 module.exports = allowedOrigins;
