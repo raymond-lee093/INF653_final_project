4b5f4b26abd3dc72be93f78c6c7d44ddb9f4e767
Raymond Lee <raymondlee093@gmail.com>
2023-04-26 14:02:34 -0500

    Modified urls in file

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://dazzling-snickerdoodle-777101.netlify.app"];
+const allowedOrigins = [
+  "https://dazzling-snickerdoodle-777101.netlify.app",
+  "http://127.0.0.1:3500",
+  "http://loclahost:3500",
+];
 
 module.exports = allowedOrigins;
